7ba2da63031d6713a1c0a4e68b899a42c0032bf5
Shefali-Sharma <shefali92@gmail.com>
2018-04-13 22:53:53 -0400

    New Files
    New Files

---
+++ db.pl
@@ -0,0 +1,33 @@
+loves(romeo, juliet).
+
+loves(juliet, romeo) :- loves(romeo, juliet).
+
+male(albert).
+male(bob).
+male(bill).
+male(carl).
+male(charlie).
+male(dan).
+male(edward).
+
+female(alice).
+female(betsy).
+female(diana).
+
+happy(albert).
+happy(alice).
+happy(bob).
+happy(bill).
+
+with_albert(alice).
+
+runs(albert) :- happy(albert).
+
+dances(alice) :- happy(alice), with_albert(alice).
+
+does_alice_dance :- dances(alice), write('When Alice is happy and with Albert she dances').
+
+swims(bob) :- happy(bob), near_water(bob).
+
+swims(bill) :- happy(bill).
+swims(bill) :- near_water(bill).
---
+++ sum.pl
@@ -0,0 +1,7 @@
+sumA(A, B) :-
+X is A + 0,
+Z is B + 0,
+write(X), nl,
+write(Z), nl,
+Y is A + B,
+write(Y), nl.
