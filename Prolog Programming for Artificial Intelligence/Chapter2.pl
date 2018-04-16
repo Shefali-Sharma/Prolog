05f3c85bb2ca772ecd6749abcb9f6430a478a0fb
Shefali-Sharma <shefali92@gmail.com>
2018-04-16 19:33:30 -0400

    Introducing Chapter 2
    Introducing Chapter 2

---
+++ Prolog Programming for Artificial Intelligence/Chapter2.pl
@@ -0,0 +1,57 @@
+
+date(D,M,Y).
+
+point(X, Y).
+point(X, Y, Z).
+
+seg(point(A, B), point(C, D)).
+
+triangle(point(A,B), point(C, D), point(E,F)).
+
+vertical( seg( point(X, Y), point(X, Y1))).
+horizontal( seg( point(X, Y), point(X1, Y))).
+
+rectangle(point(A,B), point(C, B), point(A,F), point(C,F)).
+
+f(1, one).
+f(s(1), two).
+f(s(s(1)), three).
+f(s(s(s(X))), N) :-
+  f(X, N).
+
+parent(X, Y).
+
+predecessor(X, Y) :-
+  parent(X, Y).
+predecessor(X, Y) :-
+  parent(X, Z),
+  predecessor(Z, Y).
+
+relatives(X, Y) :-
+  predecessor(X, Y);
+  predecessor(Y, X);
+  predecessor(Z, X),
+  predecessor(Z, Y);
+  predecessor(X, Z),
+  predecessor(Y, Z).
+
+
+translate(Number, Word) :-
+  Number = 1, Word = one.
+
+translate(Number, Word) :-
+  Number = 2, Word = two.
+
+translate(Number, Word) :-
+  Number = 3, Word = three.
+
+big( bear).
+big( elephant).
+smal( cat).
+
+brown( bear).
+black( cat).
+gray( elephant).
+
+dark( Z) :- black( Z).
+dark( Z) :- brown( Z).
