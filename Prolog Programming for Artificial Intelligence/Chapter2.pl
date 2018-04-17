c50ef31ec8ef2de64f2988285fa7318b237b54df
Shefali-Sharma <shefali92@gmail.com>
2018-04-17 15:21:25 -0400

    Chapter 2
    Chapter 2

--- Prolog Programming for Artificial Intelligence/Chapter2.pl
+++ Prolog Programming for Artificial Intelligence/Chapter2.pl
@@ -55,3 +55,31 @@ gray( elephant).
 
 dark( Z) :- black( Z).
 dark( Z) :- brown( Z).
+
+state(atdoor, onfloor, atwindow, hasnot).
+state(middle, onbox, middle, hasnot).
+state(middle, onbox, middle, has).
+
+move( state(middle, onbox, middle, hasnot),
+      grasp,
+      state(middle, onbox, middle, has)).
+
+move( state(P, onfloor, P, H),
+      climb,
+      state(P, onbox, P, H)).
+
+move( state(P1, onfloor, P1, H),
+      push(P1, P2),
+      state(P2, onfloor, P2, H)).
+
+move( state(P1, onfloor, B, H),
+      walk( P1, P2),
+      state(P2, onfloor, B, H)).
+
+canget(state(_, _, _, has)).
+
+canget( S1) :-
+  move( S1, M, S2),
+  canget( S2).
+
+X = f(X).
